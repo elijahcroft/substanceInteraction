995f5c8cee6a18f283fe7d9d5d299f063d4c0c45
elijah croft <elijahcroft49@gmail.com>
2025-02-10 19:19:41 -0800

    added some functionality, buttons now grey out when clicked

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     let buttons = document.querySelectorAll('.substance-button');
+    let infoBox = document.querySelector('.card-body')
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             console.log('Button clicked:');
-            this.style.opacity = '0.7';
-            this.disabled = true;
+            if(this.style.opacity === '0.7'){
+                this.style.opacity = '1';
+                
+                infoBox.innerHTML = ''
+            }
+            else{
+                this.style.opacity = '.7';
+                infoBox.innerHTML = `<p>button pressed</p>`; // Display effect info
+                
+            }
+            
+            
+            
+            
         });
     });
+
 });
